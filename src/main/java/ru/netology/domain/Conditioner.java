ef8376b5e9d5044abe4c42a373493006d11b338a
BethPech <colnze17-94@mail.ru>
2020-08-15 16:30:33 +0300

    2 tests passed2

--- src/main/java/ru/netology/domain/Conditioner.java
+++ src/main/java/ru/netology/domain/Conditioner.java
@@ -50,6 +50,7 @@ public class Conditioner {
         }
         this.currentTemperature = currentTemperature;
     }
+
     public void setDecreaseCurrentTemperature(int currentTemperature) {
         if (currentTemperature > maxTemperature) {
             return;
@@ -73,95 +74,4 @@ public class Conditioner {
         this.on = true;
 
     }
-
-//
-//    public void setCurrentTemperature(int CurrentTemperature) {
-//        if (currentTemperature < maxTemperature) {
-//            return;
-//        }
-//        if (currentTemperature > minTemperature) {
-//            return;
-//        }
-//        while (currentTemperature <= maxTemperature) {
-//            System.out.println(+currentTemperature--);
-//        }
-//    }
-
-
-//        for(int i = 0; i >= maxTemperature; i++) {
-//            return;
-//            for (int i = 0, i < minTemperature; i--){
-//                return;
-//            }
-//            System.out.println(i);
-//    }
-
 }
-
-
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public int getMaxTemperature() {
-//        return maxTemperature;
-//    }
-//
-//    public void setMaxTemperature(int maxTemperature) {
-//        this.maxTemperature = maxTemperature;
-//    }
-//
-//    public int getMinTemperature() {
-//        return minTemperature;
-//    }
-//
-//    public void setMinTemperature(int minTemperature) {
-//        this.minTemperature = minTemperature;
-//    }
-//
-//    public int getCurrentTemperature() {
-//        return currentTemperature;
-//    }
-//
-//    public boolean isOn() {
-//        return on;
-//    }
-//
-//    public void setOn(boolean on) {
-//        this.on = on;
-//    }
-//
-//    public void increaseCurrentTemperature(int currentTemperature) {
-//
-//        if (currentTemperature > maxTemperature) {
-//            return;
-//        }
-//        if (currentTemperature < minTemperature) {
-//            return;
-//        }
-//        // здесь уверены, что все проверки прошли
-//        this.currentTemperature = currentTemperature;
-//        for (currentTemperature < maxTemperature; currentTemperature++) ;
-//        System.out.println(currentTemperature);
-//    }
-//
-//    public void decreaseCurrentTemperature(int currentTemperature) {
-//        if (currentTemperature > maxTemperature) {
-//            return;
-//        }
-//        if (currentTemperature < minTemperature) {
-//            return;
-//        }
-//        // здесь уверены, что все проверки прошли
-//        this.currentTemperature = currentTemperature;
-//        for (currentTemperature > minTemperature; currentTemperature--)
-//            System.out.println(currentTemperature);
-//    }
-//
-//}
-//
-
